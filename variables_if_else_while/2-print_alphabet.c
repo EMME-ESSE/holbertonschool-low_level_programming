f6359a79348a3943e8afbe303be8e1d6037d0048
EMME-ESSE <matimancini@gmail.com>
2022-09-27 09:03:38 -0700

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *    *     */
+int main()  
+{  
+	    char ch = 'a';  
+	      
+	        printf("Lowercase English Alphabets:\n");  
+		    while(ch <= 'z')  
+			        {  
+					        printf("%c ", ch);  
+						        ch++;  
+							    }  
+		        printf("\n");  
+			  
+			    return 0;  
+}  
