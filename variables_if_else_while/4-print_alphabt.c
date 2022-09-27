79c45555a7bca505a0c6dd0ff4627b8fe8663fac
EMME-ESSE <matimancini@gmail.com>
2022-09-27 09:26:36 -0700

    1}

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  *  * main - Entry point
+ *   *    * Return: Always 0 (Success)
+ *   **/
+int main(void)
+{
+e = "e";
+q = "q";
+	char low;
+	for (low = 'a'; low <= 'z'; low++)	{
+		if (low != e && low != q)
+		putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
