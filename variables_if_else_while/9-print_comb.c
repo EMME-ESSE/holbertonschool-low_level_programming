98ca5257543f0af3a0af7baf1ffa92ab1a6572da
EMME-ESSE <matimancini@gmail.com>
2022-09-28 08:32:53 -0700

    1

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -2,18 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-** main - Entry point
-** Return: Always 0 (Success)
+ *  * main - Entry point
+ *    * Return: Always 0 (Success)
 **/
 int main(void)
 {
-int temp, i, n;
-for (i = 0; i < 10; i++)
+int d;
+for (d = '0'; d <= '9'; d++)
 {
-temp = num[i];
-num[i] = num[i + 1];
-num[i + 1] = temp;
-print(num, n);
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
 }
+putchar('\n');
 return (0);
 }
