cd9f811e461a21f301c51a3fa61888a88a0377f5
EMME-ESSE <matimancini@gmail.com>
2022-09-28 08:37:51 -0700

    2

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -4,18 +4,21 @@
 /**
  *  * main - Entry point
  *    * Return: Always 0 (Success)
-**/
+ *     */
 int main(void)
 {
-int d;
-for (d = 0 ; d <=  9 ; d++)
-{
-putchar(d);
-if (d !=  9)
-{
-putchar(',');
-putchar(' ');
-}
-putchar('\n');
-return (0);
+		int d;
+
+			for (d = '0'; d <= '9'; d++)
+					{
+								putchar(d);
+										if (d != '9')
+													{
+																	putchar(',');
+																				putchar(' ');
+																						}
+											}
+				putchar('\n');
+
+					return (0);
 }
