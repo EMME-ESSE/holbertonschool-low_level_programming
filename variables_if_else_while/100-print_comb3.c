825eea68049b564f10352e22ac19d5691300d99d
EMME-ESSE <matimancini@gmail.com>
2022-09-28 08:55:08 -0700

    1

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *  *  * main - Entry point
- *   *    * Return: Always 0 (Success)
- *   **/
+*  * main - Entry point
+*    * Return: Always 0 (Success)
+**/
 int main(void)
 {
-	int d;
-	for (d = '10'; d <= '99'; d++)
-	{
-		putchar(d);
-		if (d != '99')
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+int d;
+for (d = '10'; d <= '99'; d++)
+{
+putchar(d);
+if (d != '99')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
