8b08ae4233ce91e63e34a1ec55ca008ec90993cd
EMME-ESSE <matimancini@gmail.com>
2022-09-28 07:43:08 -0700

    1

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  *  * main - Entry point
+ *   *    * Return: Always 0 (Success)
+ *   **/
+void main()
+{
+int temp, i, n;
+for (i = 0; i < 10; i++)
+{
+temp = num[i];
+num[i] = num[i+1];
+num[i+1] = temp;
+print(num, n);
+}
+return 0;
+}
