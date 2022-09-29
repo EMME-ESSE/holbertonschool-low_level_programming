d5f588aa76825f4523678a4b8c76f9a6b1dd1b26
EMME-ESSE <matimancini@gmail.com>
2022-09-29 06:15:40 -0700

    2

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,10 @@
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
  *  * print_alphabet - Check description
  *   * Description: It prints the alphabet in lowercase fallowed by a new line
  *    * Return: Nothing.
 **/
-void print_alphabet()
+void print_alphabet(void)
 {
 char i;
 for (i = 'a'; i <= 'z'; i++)
