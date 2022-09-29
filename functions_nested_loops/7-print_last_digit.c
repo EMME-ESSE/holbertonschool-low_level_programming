20d59f6e5d0bd33d2e94ab0ccc0ef13a8e930986
EMME-ESSE <matimancini@gmail.com>
2022-09-29 15:45:20 -0700

    2

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * ** print_last_digit - Computes the absolute value of an integer.
+ * ** @n: Prints the las digits
+ * ** Return: 0.
+**/
+int print_last_digit(int);
+{
+	    int r;
+
+	        print_last_digit(98);
+		    print_last_digit(0);
+		        r = print_last_digit(-1024);
+			    _putchar('0' + r);
+			        _putchar('\n');
+				    return (0);
+}
