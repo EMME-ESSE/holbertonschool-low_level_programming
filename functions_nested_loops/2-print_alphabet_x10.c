5baa2c6da610a28702d87bd50b8bac2c7b0b37cd
EMME-ESSE <matimancini@gmail.com>
2022-09-29 06:38:36 -0700

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  * print_alphabet - Check description
+ *   * Description: It prints the alphabet in lowercase fallowed by a new line
+ *    * Return: Nothing.
+**/
+void print_alphabet_x10(void);
+{
+int a;	
+char word[8] = "_putchar";
+int i;
+for (a = 1; a < 10; a++){
+for (i = 0; i < 8; i++)
+	putchar(word[i]);
+putchar('\n');
+}
+}
