74bc311a03ae3604c2332b2ec10c40ffafd2a6ee
EMME-ESSE <matimancini@gmail.com>
2022-09-29 06:28:51 -0700

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,4 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 /**
  *  * print_alphabet - Check description
  *   * Description: It prints the alphabet in lowercase fallowed by a new line
