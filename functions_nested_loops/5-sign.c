314454fcc35d95050d97c7e51f5868ce88546fac
EMME-ESSE <matimancini@gmail.com>
2022-09-29 09:05:59 -0700

    1

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *  * _isalpha - Check Holberton
+ *   * @c: An input character
+ *    * Description: function returns 1 if the character is a
+ *     * letter, lowercase or uppercase.
+** Return: 1 if n is greather than 0, 0 if n = 0 and -1 if n is less than 0.
+**/
+int print_sign(int n);
+{
+int num = 0;
+if (n > 0)
+{
+_putchar('+');
+num = 1;
+}
+else if (n < 0)
+{
+_putchar('-')
+num = -1
+}
+else
+{
+_putchar('0')
+return(num)
+}
