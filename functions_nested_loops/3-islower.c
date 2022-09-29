267dec3342f3ca8d4fbcdabf02683ce792051c4e
EMME-ESSE <matimancini@gmail.com>
2022-09-29 07:44:17 -0700

    1

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *  * print_alphabet_x10 - Check holberton
+ *   * Description: function uses _putchar function to print
+ *    * alphabet in lowercase 10 times
+ *     * Return: Nothing.
+**/
+
+int _islower(int c);
+{
+int c = 'a';
+if (islower(c))
+{
+	return(1);
+}
+else
+{
+	return(0);
+}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet_x10(void);
+int _islower(int c);
