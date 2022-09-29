d124b780a976eb9a3805eb001c588e792f7b7136
EMME-ESSE <matimancini@gmail.com>
2022-09-29 06:12:38 -0700

    2

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,10 +3,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
+ *  * print_alphabet - Check description
+ *   * Description: It prints the alphabet in lowercase fallowed by a new line
+ *    * Return: Nothing.
 **/
 void print_alphabet(void)
 {
