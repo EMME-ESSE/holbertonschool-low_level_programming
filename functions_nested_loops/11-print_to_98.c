d3d620808bf20a85b85426ef8ea58b0a0786e256
EMME-ESSE <matimancini@gmail.com>
2022-09-29 17:06:28 -0700

    1

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,15 +8,15 @@ void print_to_98(int n)
 if (n < 98)
 {
 _putchar(n);
-_putchar(",");
-_putchar(" ");
+_putchar(',');
+_putchar(' ');
 n++;
 }
 else if (n > 98)
 {
 _putchar(n);
-_putchar(",");
-_putchar(" ");
+_putchar(',');
+_putchar(' ');
 n--;
 }
 else 
