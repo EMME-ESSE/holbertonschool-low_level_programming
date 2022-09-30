272c9ae9d10ea193b99529799a1fa735693cd560
EMME-ESSE <matimancini@gmail.com>
2022-09-30 09:48:43 -0700

    2

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,15 +6,15 @@
 **/
 int print_last_digit(int n)
 {
-		if (n >= 0)
-				{
-							n = n % 10;
-									_putchar('0' + n);
-										}
-			else
-					{
-								n = (n % 10) * -1;
-										_putchar('0' + n);
-											}
-				return (n);
+if (n >= 0)
+{
+n = n % 10;
+_putchar('0' + n);
+}
+else
+{
+n = (n % 10) * -1;
+_putchar('0' + n);
+}
+return (n);
 }
