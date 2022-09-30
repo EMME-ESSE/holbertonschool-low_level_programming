17d93b436b2baabbb4487cf27d50538b73b8d46c
EMME-ESSE <matimancini@gmail.com>
2022-09-30 08:23:29 -0700

    1

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * ** print_last_digit - Print the las digits of few numbers.
- * ** @n: Prints the last digit
- * ** Return: 0.
+** print_last_digit - Print the las digits of few numbers.
+** @r: Prints the last digit
+** Return: 0.
 **/
 int print_last_digit(int r)
 {
