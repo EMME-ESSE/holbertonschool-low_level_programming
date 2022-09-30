0b7b6f46d9608351b5e713c93f78e9d7df68c46d
EMME-ESSE <matimancini@gmail.com>
2022-09-30 06:25:34 -0700

    1

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -3,8 +3,10 @@
  * times_table - Prints the 9 times table.
  *
 **/
-void times_table(int tb, int mul)
+void times_table(void)
 {
+	int tb;
+	int mul;
 for (tb = 1; tb <= 10; tb++)
 {
 for (mul = 1; mul <= 10; mul++)
