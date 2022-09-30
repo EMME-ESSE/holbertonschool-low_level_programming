9a2036738ad8fbc4009e998127983ebc5b3b65b4
EMME-ESSE <matimancini@gmail.com>
2022-09-30 10:02:03 -0700

    1

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,20 +5,20 @@
  *   **/
 void times_table(void)
 {
-	int tb;
-	int mul;
-	for (tb = 0; tb <= 9; tb++)
-	{
-		printf ("\n");
-		{
-			for (mul = 0; mul <= 9; mul++)
-			{
-				int tot = tb*mul;
-				if ((tot / 10) == 0)
-					(printf ("%d,  ", tot);
-				else 
-				printf ("%d, ", tot);
-		}
-	}
+int tb;
+int mul;
+int tot = tb*mul;
+for (tb = 0; tb <= 9; tb++)
+{
+printf ("\n");
+{
+for (mul = 0; mul <= 9; mul++)
+{
+if ((tot / 10) == 0)
+(printf ("%d,  ", tot);
+else 
+printf ("%d, ", tot);
+}
+}
 }
 }	
