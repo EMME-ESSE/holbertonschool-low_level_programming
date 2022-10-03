67b88bbb0a6f58c922e9518c9cc3eca92a3b2575
EMME-ESSE <matimancini@gmail.com>
2022-10-03 09:48:41 -0700

    1

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  * print_numbers - Prints the numbers from 0-9.
+**/
+void print_most_numbers(void)
+{
+		if (c >= '0' && c <= '9')
+			if (c != 4 || c != 2)
+				return (1);
+		else
+			return (0);
+
+}
