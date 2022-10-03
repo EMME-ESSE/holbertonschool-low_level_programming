cda058cd61f679ab15a68255e0340a2d983f33bd
EMME-ESSE <matimancini@gmail.com>
2022-10-03 09:38:00 -0700

    1

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  * print_numbers - Multiples two integers.
+ *   * @a: Number to print.
+ *      * Return: The result of the multiplication.
+**/
+void print_numbers(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+_putchar((a % 10) + '0');
+_putchar('\n');
+}
+}
