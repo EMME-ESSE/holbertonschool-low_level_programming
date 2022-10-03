2f62ed46de83ebbadcb454802c995e0e27070aef
EMME-ESSE <matimancini@gmail.com>
2022-10-03 10:11:55 -0700

    1

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,14 @@ void more_numbers(void)
 {
 int a, b;
 
-for (a = 0; a < 11; a++)
+for (a = 0; a <= 9; a++)
 {
 for (b = 0; b <= 14; b++)
 {
-	if (b > 9)
-						_putchar((b / 10) + '0');
-				_putchar((b % 10) + '0');
-						}
-		_putchar('\n');
-			}
+if (b > 9)
+_putchar((b / 10) + '0');
+_putchar((b % 10) + '0');
+}
+_putchar('\n');
+}
 }
