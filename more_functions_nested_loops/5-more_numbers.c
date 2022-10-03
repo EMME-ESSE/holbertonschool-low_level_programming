f1159dd179073186089e5e7cdef84bbc635ac57a
EMME-ESSE <matimancini@gmail.com>
2022-10-03 10:05:22 -0700

    2

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,10 +5,11 @@
 void more_numbers(void)
 {
 int a, b;
+
 for (a = 0; a < 11; a++)
 {
-	for (b = 0; b <= 14; b++)
-	_putchar(b + '0');
+for (b = 0; b <= 14; b++)
+_putchar(b + '0');
 }
 _putchar('\n')
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,3 +7,4 @@ int _isdigit(int c);
 int _putchar(char c);
 int _isupper(int c);
 void print_most_numbers(void);
+void more_numbers(void);
