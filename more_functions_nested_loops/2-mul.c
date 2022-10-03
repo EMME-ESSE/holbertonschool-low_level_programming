374f8508cd9f9958b8e40972f301553d4233a252
EMME-ESSE <matimancini@gmail.com>
2022-10-03 09:25:56 -0700

    1

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *  *  * print_alphabet - Check description
- *   *   * Description: It prints the alphabet in lowercase fallowed by a new line
- *    *    * Return: 0.
+ *  *print_alphabet - Check description
+ *   * Description: It prints the alphabet in lowercase fallowed by a new line
+ *    *Return: 0.
 **/
 int mul(int a, int b)
 {
