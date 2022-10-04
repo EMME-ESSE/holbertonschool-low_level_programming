6cac4392275b5bac01ece40d06bf1765fab924cf
EMME-ESSE <matimancini@gmail.com>
2022-10-04 13:30:35 -0700

    pushed

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,12 +7,12 @@ int main(void)
 int fizz;
 for (fizz = 1; fizz <= 100; fizz++)
 {
-if ((fizz % 3) == 0)
+if ((fizz % 3) == 0 && (fizz % 5) == 0)
+printf("FizzBuzz");
+else if ((fizz % 3) == 0)
 printf("Fizz");
 else if ((fizz % 5) == 0)
 printf("Buzz");
-else if ((fizz % 5) == 0 && (fizz % 3) == 0)
-printf("FizzBuzz");
 else
 printf("%d", fizz);
 if (fizz == 100)
