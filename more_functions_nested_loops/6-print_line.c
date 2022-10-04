3a8774279433e434ce73602a0048ef1a3af8d164
EMME-ESSE <matimancini@gmail.com>
2022-10-04 05:44:04 -0700

    1

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*  * print_line - Prints lines depending to the input "n" .
+**/
+void print_line(void)
+{
+int l;
+if (n > 0)
+{
+for (l = 0; l < n; l++)
+_putchar('_');
+}
+_putchar('\n');
+}
