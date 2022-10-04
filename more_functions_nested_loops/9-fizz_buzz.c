b7356cced9e321a8afce85733cb67a894ec5d830
EMME-ESSE <matimancini@gmail.com>
2022-10-04 13:19:31 -0700

    ex 9 task 5

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,15 +1,24 @@
 #include "main.h"
 /**
-  *  * main - Prints numbers from 0 to 100 with 'fizz' in * 3 or * 5.
+  *  * main - Prints numbers from 0 to 100 with a phrase depending on * 3 or * 5 or both.
 **/
 void main(void)
 {
 int fizz;
-for (fizz = 0; fizz < 100; fizz++)
+for (fizz = 1; fizz < 100; fizz++)
 {
-if (fizz % 3 = 0 || fizz % 5 = 0)
-_putchar('fizz');
+if ((fizz % 3) == 0)
+_putchar('Fizz');
+else if ((fizz % 5) == 0)
+_putchar('Buzz');
+else if ((fizz % 5) == 0 && (fizz % 3) == 0)
+_putchar('FizzBuzz');
 else
-_putchar(fizz);
+_puchar(fizz + '0');
 }
+if (fizz == 100)
+continue;
+_putchar(' ');
+_putchar('\n');
+return(0);
 }
