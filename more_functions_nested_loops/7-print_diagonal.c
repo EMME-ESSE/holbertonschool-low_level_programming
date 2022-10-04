ffe3285e8bd1a1eaa3b4da430ada4de9b8fa40bf
EMME-ESSE <matimancini@gmail.com>
2022-10-04 05:51:04 -0700

    pushed

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *  * print_line - Prints lines depending to the input "n" .
+ * **/
+void print_diagonal(void)
+{
+	int l;
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+			_putchar('\');
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
