996abf91b2d27a91a11474391be009b89d0abfc6
EMME-ESSE <matimancini@gmail.com>
2022-10-06 07:36:30 -0700

    task2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  *  * _strlen - Writes the length of a string.
- *   * @s: The length of the string .
+ *   * @str: The length of the string .
  *    * Return: 0 
- *     */
+**/
 size_t _strlen(const char *str)
 {
 size_t length = 0;
