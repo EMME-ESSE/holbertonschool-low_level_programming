b0b44a2e4668fc947c9d4a622422fed585849632
EMME-ESSE <matimancini@gmail.com>
2022-10-06 07:14:13 -0700

    task1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+ *  * swap_int - Swap two pointers values.
+ *   *  @a: First pointer to be swaped
+ *    * @b: Second pointer.
+**/
+void swap_int(int *a, int *b);
+{
+int c;
+c = *a;
+*a = *b;
+*b = c;
+}
