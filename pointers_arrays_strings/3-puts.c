ab030a4ddacdc60b531572e643be686a6faafb1b
EMME-ESSE <matimancini@gmail.com>
2022-10-06 08:57:15 -0700

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *  *  * _strlen - Writes the length of a string.
+ *   *   * @str: The length of the string .
+ *    *    * Return: 0
+ *    **/
+void _puts(char *str)
+{
+	puts(*str);
+	return(0);
+}
