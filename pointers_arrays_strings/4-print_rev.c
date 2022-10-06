8742e51cf96255a2a7e5db75c386e590e7b37cbc
EMME-ESSE <matimancini@gmail.com>
2022-10-06 09:23:04 -0700

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ *  *  * _strlen - Writes the length of a string.
+ *   *   * @s: The length of the string .
+ *    *    * Return: 0
+**/
+void print_rev(char *s)
+{
+ printf("%s", s);
+}
