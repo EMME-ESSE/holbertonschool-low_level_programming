3ec670a1c797b4031335141a43f810020da6dc98
EMME-ESSE <matimancini@gmail.com>
2022-10-06 09:42:47 -0700

    pushed

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  *  * _strlen - Writes the length of a string.
+ *   *   * @s: The length of the string .
+ *    *    * Return: 0
+**/
+void rev_string(char *s)
+{
+int len = 0;
+char cas;
+while (s[len] != '\0')
+	len++;
+while (len)
+cas = (s[--len]);
+printf("%d", cas);
+}
