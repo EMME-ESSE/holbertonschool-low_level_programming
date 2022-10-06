70f0ccf09c58cd8b42f6087b4a93344e850b0f6e
EMME-ESSE <matimancini@gmail.com>
2022-10-06 12:34:20 -0700

    task 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,7 +3,7 @@
  *  *  * _strlen - Writes the length of a string.
  *   *   * @str: The length of the string.
  *    **/
-void_putss(char *str)
+void _puts(char *str)
 {
 	while(*str)
 	_putchar(*str++);
