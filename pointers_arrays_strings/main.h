12a22cce1188596722be35f95ddf26953ac9b6ff
EMME-ESSE <matimancini@gmail.com>
2022-10-06 13:03:11 -0700

    pushed

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
-void rev_string(char *s)
+void rev_string(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
