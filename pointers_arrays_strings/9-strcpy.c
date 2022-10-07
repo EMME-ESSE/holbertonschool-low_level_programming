7a919aae7f6c0c28030824c16cbef5a2caac1da6
EMME-ESSE <matimancini@gmail.com>
2022-10-07 08:31:49 -0700

    pushed

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,14 @@
 #include "main.h"
+/**
+ * *  *_strcpy - copies the string pointed to by src.
+ * *  @dest: A pointer to destination of string.
+ *  * @src: A pointer to source string to copy from.
+ *    * Return: pointer to de
+**/
+char *_strcpy(char *dest, char *src)
+{
+char *ayuda = dest;
+while (*src)
+*dest++ = *src++;
+return (ayuda);
+}
