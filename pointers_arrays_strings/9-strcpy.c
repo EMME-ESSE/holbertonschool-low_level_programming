bd0a5d9406abf75ba9f6ab1c213562d0ecce3e8d
EMME-ESSE <matimancini@gmail.com>
2022-10-07 15:33:45 -0700

    task 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,16 @@
  *        */
 char *_strcpy(char *dest, char *src)
 {
-int index = 0;
-while (src[index])
+int len, i;
+len = 0;
+while (src[len] != '\0')
 {
-dest[index] = src[index];
-index++;
+len++;
 }
+for (i = 0; i < len; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
 return (dest);
 }
