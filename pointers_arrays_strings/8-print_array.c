c25506d89af77bcc4ce9a6e6650f6a585c78449f
EMME-ESSE <matimancini@gmail.com>
2022-10-07 07:41:07 -0700

    task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -6,7 +6,12 @@
  *    **/
 void print_array(int *a, int n)
 {
-int array[i];
+int i;
 for (i = 0; i < n; i++)
-printf("%d, ",n[i]);
+{
+printf("%d",a[i]);
+if (i < n - 1)
+printf(", ");
+}
+putchar('\n');
 }
