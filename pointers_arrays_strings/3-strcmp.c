c03f84fc14ebc9ff2e2bef3387d067e49c27a7a7
EMME-ESSE <matimancini@gmail.com>
2022-10-10 08:44:37 -0700

    task 3 ex2

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,9 +3,11 @@
  ** _strcmp - Compares pointers to two strings.
  ** @s1: A pointer to the first string to be compared.
  ** @s2: A pointer to the second string to be compared.
- ** Return: If str1 < str2, the negative difference of the unmatched characters.
- **        If str1 == str2, the result will return 0.
- **       If str1 > str2, the positive difference of the unmatched characters.
+ ** Return: If str1 < str2, the negative difference of
+ **         the unmatched characters.
+ **         If str1 == str2, the result will return 0.
+ **         If str1 > str2, the positive difference of the
+ **         unmatched characters.
 **/
 int _strcmp(char *s1, char *s2)
 {
