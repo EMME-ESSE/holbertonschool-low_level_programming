508a8594dbc6ff843cac28d11663a433e61a41c0
EMME-ESSE <matimancini@gmail.com>
2022-10-10 08:48:39 -0700

    task 4 ex5

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -7,11 +7,11 @@
  **/
 void reverse_array(int *a, int n)
 {
-		int z, i;
-			for (i = n - 1; i >= n / 2; i--)
-					{
-						z = a[n - 1 - i];
-										a[n - 1 - i] = a[i];
-												a[i] = z;
-													}
+int z, i;
+for (i = n - 1; i >= n / 2; i--)
+{
+z = a[n - 1 - i];
+a[n - 1 - i] = a[i];
+a[i] = z;
+}
 }
