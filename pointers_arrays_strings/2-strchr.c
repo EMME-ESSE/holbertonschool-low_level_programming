5543f1b352f6a5374d061e721b1a3094576fe5c7
EMME-ESSE <matimancini@gmail.com>
2022-10-11 08:36:43 -0700

    task 2 ex 3

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *  * _strchr - a function that copys memory area
+ *   * @s: memory area destination to copy
+ *    * @c: memory area source to copy from
+ *      * Return: returns pointer to copy location
+**/
+char *_strchr(char *s, char c)
+{
+int len, i;
+char *f;
+len = strlen(s);
+*s = c
+for (i = 0; i < len; i++)
+{
+if (c[i] == s[i] && c[i] == NULL)
+{
+printf("%s", c[i]);
+}
+if (c[i] == s[i] && c[i] != NULL)
+{
+printf("%s", c[i]);
+}
+}
+}
