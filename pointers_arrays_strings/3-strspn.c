30a891a7ce337916097ff0e0143c85335c83aa6b
EMME-ESSE <matimancini@gmail.com>
2022-10-13 07:32:58 -0700

    task 3 ex 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,26 @@
 #include "main.h"
 /**
- * **_strspn - Gets the length of a prefix substring.
- * ** @s: The String to be calculated
- * ** @accept: the prefix
- * ** Return: A length.
- * **/
+** _strspn - Gets the length of a prefix substring.
+** @s: The String to be calculated
+** @accept: the prefix
+** Return: A length.
+**/
 unsigned int _strspn(char *s, char *accept)
 {
-		unsigned int num;
-			int index;
-				while(*s)
-						{
-									for (index = 0; accept[index]; index++)
-												{
-																if (*s == accept[index])
-																				{
-																									num++;
-																													break;
-																																}
-																			else if(accept != 'NULL')
-																							return(num)
-																										}
-											s++;
-												}
-				return(num);
+unsigned int num;
+int index;
+while(*s)
+{
+for (index = 0; accept[index]; index++)
+{
+if (*s == accept[index])
+{
+num++;
+break;
+}
+else if(accept != '\0')												return(num);
+}
+s++;
+}
+return(num);
 }
