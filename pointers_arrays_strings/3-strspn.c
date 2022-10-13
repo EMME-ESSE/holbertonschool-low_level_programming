7ec8d927b9dd5001c5604168fe880ff1d4b6ad0b
EMME-ESSE <matimancini@gmail.com>
2022-10-13 08:48:29 -0700

    task 3 ex 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,7 @@
 ** _strspn - Gets the length of a prefix substring.
 ** @s: The String to be calculated
 ** @accept: the prefix
-** Return: A length.
+** Return: The length.
 **/
 unsigned int _strspn(char *s, char *accept)
 {
