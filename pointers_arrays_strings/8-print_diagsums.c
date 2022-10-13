436c96587f95232eed22982f1d7394ed87d78c15
EMME-ESSE <matimancini@gmail.com>
2022-10-13 12:22:18 -0700

    task 8 ex 3

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -5,7 +5,7 @@
  *   *                  of a square matrix of integers.
  *    * @a: The matrix of integers.
  *     * @size: The size of the matrix.
- *      */
+**/
 void print_diagsums(int *a, int size)
 {
 int index, sum1 = 0, sum2 = 0;
