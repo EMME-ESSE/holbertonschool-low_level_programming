46345f75fc12232f9133cfd246cfaaab8bed5187
EMME-ESSE <matimancini@gmail.com>
2022-10-14 06:48:05 -0700

    task 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -3,7 +3,7 @@
 ** _print_rev_recursion - Prints a string in reverse
 ** @s: the string
 **/
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
-	_print_rev_recursion(s + 1);
+_print_rev_recursion(s + 1);
 }
