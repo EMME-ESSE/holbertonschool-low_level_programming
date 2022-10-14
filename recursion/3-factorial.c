3d967774a08b692159a486b3eb577f52b8c30538
EMME-ESSE <matimancini@gmail.com>
2022-10-14 08:47:28 -0700

    task 3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+** factorial - a function that returns the factorial of a number.
+** @n: The int
+** Return: The length of the string
+**/
+int factorial(int n)
+{
+if (n == 0)
+return 1;
+if ((n * n-1) < 0)
+return -1;
+}
