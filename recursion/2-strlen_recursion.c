fd98689fc3d235e27da7a02a8cac4a93ab2fdb1a
EMME-ESSE <matimancini@gmail.com>
2022-10-14 07:17:10 -0700

    .

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  * _strlen_recursion  - a function that returns the length of a string.
+ *   * followed by a new line.
+ *    * @s: An input string to printing
+ *     * Return: Nothing
+**/
+int _strlen_recursion(char *s)
+{
+	int i;
+	if(s[i]=='\0')
+		        return 0;
+	    else
+		            return (1+_strlen_recursion(s,i+1));
+}
