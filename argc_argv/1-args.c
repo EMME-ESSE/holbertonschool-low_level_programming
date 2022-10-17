ca1d6155c21552d72335d4610a27e4f72e362e50
EMME-ESSE <matimancini@gmail.com>
2022-10-17 06:21:14 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+/**
+ *  * main - Prints the number of arguements passed.
+ *   * @argc: An argument counter
+ *    * @argv: An argument values
+ *     * Return: Always 0 (Success)
+**/
+int main(int argc, char *argv[])
+{
+if (argv[0])
+printf("%d\n", argc - 1);
+return (0);
+}
