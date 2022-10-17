0df4ed139da62e974b19eaa806c993831ba418bb
EMME-ESSE <matimancini@gmail.com>
2022-10-17 06:07:16 -0700

    pushed

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - rename the file.
+ *
+**/
+void main(int argc, char *argv[])   
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s ", argv[i]);       
+}
+printf("\n");
+}
