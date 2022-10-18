c69fb180d69970852b88968e6048e947c12ed05a
EMME-ESSE <matimancini@gmail.com>
2022-10-18 06:19:31 -0700

    task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -5,15 +5,12 @@
  *    * @argv: This is the array of argument 
  *      * Return: Zero upon program success
 **/
-
 int main(int argc, char *argv[])
 {
 int i;
 int result = 1;
 for (i = 1; i < argc; i++)
-{
-result = result * argv[i];
-}
+result = (result * argv[i]);
 printf("%d\n", result);
 return (0);
 }
