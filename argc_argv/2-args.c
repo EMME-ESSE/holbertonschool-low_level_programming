ce88b046f0dda6903713908909bdea101f5f8359
EMME-ESSE <matimancini@gmail.com>
2022-10-18 05:13:30 -0700

    pushed

---
+++ argc_argv/2-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  *  * main - Prints the arguments recived.
+ *   *   * @argc: An argument counter
+ *    *    * @argv: An argument values
+ *     *     * Return: Always 0 (Success)
+ *     **/
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%d\n", argv + 1);
+	return (0);
+}
