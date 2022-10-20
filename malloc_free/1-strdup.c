8b2968ef34821d45ae0d860a47e1bd1c97277c7e
EMME-ESSE <matimancini@gmail.com>
2022-10-20 06:56:50 -0700

    pushed

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -6,12 +6,12 @@ char *_strdup(char *str)
 {
 	unsigned int i;
 	char *string2;
-	string2 = malloc(sizeof(char) * strlen(str) + 1);
+	string2 = malloc(sizeof(char) * _strlen(str) + 1);
 	if (str == NULL)
 		return(NULL);
 	if (string2 != NULL && str != NULL)
 	{			
-		for (i = 0; i <= strlen(str) + 1; i++)
+		for (i = 0; i <= _strlen(str) + 1; i++)
 			string2 += str[i];
 	}
 	return (string2);
