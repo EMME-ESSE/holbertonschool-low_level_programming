3dc64059ab385a78443be48d7429f9c67228c231
EMME-ESSE <matimancini@gmail.com>
2022-10-20 07:14:13 -0700

    task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -4,18 +4,19 @@
  *  allocated space in memory, which contains a copy
  *   of the string given as a parameter
  *   @str: The first string
- * */
+**/
 char *_strdup(char *str)
 {
 	unsigned int i;
 	char *string2;
 	string2 = malloc(sizeof(char) * strlen(str) + 1);
+
 	if (str == NULL)
-		return(NULL);
+		return (NULL);
 	if (string2 == NULL)
 		free(string2);
 	else
-	{			
+	{
 		for (i = 0; i <= strlen(str) + 1; i++)
 			string2[i] += str[i];
 	}
