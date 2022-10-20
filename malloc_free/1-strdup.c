12c1aab136a9796668775c5f05da5f4d3862e8da
EMME-ESSE <matimancini@gmail.com>
2022-10-20 07:04:28 -0700

    task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,32 +1,17 @@
 #include "main.h"
 /**
- *  *  * _strlen - Writes the length of a string.
- *   *   * @s: The length of the string .
- *    *    * Return: 0
- *    **/
-int _strlen(char *s)
-{
-	int len = 0;
-	while (s[len] != '\0')
-		len++;
-	return (len);
-}
-
-
-
-/**
  *
  * */
 char *_strdup(char *str)
 {
 	int i;
 	char *string2;
-	string2 = malloc(sizeof(char) * _strlen(str) + 1);
+	string2 = malloc(sizeof(char) * strlen(str) + 1);
 	if (str == NULL)
 		return(NULL);
-	if (string2 != NULL && str != NULL)
+	else
 	{			
-		for (i = 0; i <= _strlen(str) + 1; i++)
+		for (i = 0; i <= strlen(str) + 1; i++)
 			string2 += str[i];
 	}
 	return (string2);
