11f88aa12bfcd4fa9b4ffcf72699c5ff7d3c5fc1
EMME-ESSE <matimancini@gmail.com>
2022-10-20 09:45:13 -0700

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /*
- *
+ **str_concat - Write a function that concatenates two strings
+ * @s1: first string 
+ * @s2: second string
+ * Return: 0
 */
 char *str_concat(char *s1, char *s2)
 {
