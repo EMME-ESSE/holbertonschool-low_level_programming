5afacc3e59fc2a2f3fa98d79a0bfb1575141ed59
EMME-ESSE <matimancini@gmail.com>
2022-10-21 06:40:14 -0700

    task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -25,7 +25,8 @@ int **alloc_grid(int width, int height)
 		if (puntero[i] == NULL)
 		{
 			for (; i >= 0; i--)
-				free(pointer[i];
+				free(pointer[i]);
+		
 			free(pointer);
 			return (NULL);
 		}
