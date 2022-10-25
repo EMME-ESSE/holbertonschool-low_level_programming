eb241f5655699edf71f4717b77479fe98e86b4ca
EMME-ESSE <matimancini@gmail.com>
2022-10-25 07:43:09 -0700

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,15 +18,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
          if (s2 == NULL)
 		return ("");
 	size = strlen(s1) + strlen(s2);
-	pointer = malloc(sizeof(char) * size + 1);
 	if (pointer == NULL)
 		return (NULL);
-	if (s1 == NULL)
-		return ("");
-        if (s2 == NULL)
-	        return ("");
 	for (i = 0; s1[i] != '\0' ; i++)
 	{
+		pointer = malloc(sizeof(char) * size + 1);
 		pointer[i] = s1[i];
 		sizes1++;
 	}
