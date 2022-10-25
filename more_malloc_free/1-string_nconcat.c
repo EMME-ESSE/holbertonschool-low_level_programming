5f71d9007b8f8ebc558f22c3420b4ad9d9459825
EMME-ESSE <matimancini@gmail.com>
2022-10-25 06:47:56 -0700

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -19,9 +19,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	size = strlen(s1) + strlen(s2);
 	if (n < strlen(s2))
-		pointer = malloc(sizeof(char) * (strlen(s1) + n) + 1);
+		pointer = malloc(sizeof(char) * (strlen(s1) + n);
 	if (n >= strlen(s2))
-		pointer = malloc(sizeof(char) * size + 1);
+		pointer = malloc(sizeof(char) * size);
 	if (pointer == NULL)
 		return (NULL);
 	for (i = 0; s1[i]; i++)
