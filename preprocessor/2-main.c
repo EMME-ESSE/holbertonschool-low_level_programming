9112986a9630643669335556899cb6e8c0cecca4
EMME-ESSE <matimancini@gmail.com>
2022-10-25 16:44:10 -0700

    task 2

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/**
+ *  * main - Prints the name of the file of the program
+ *   *        was compiled from, followed by a new line.
+**/
 int main(void)
 {
 	printf("%s\n", __FILE__);
