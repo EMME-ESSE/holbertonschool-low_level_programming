c0b1a9591c5ae545f263dedf9ce4a54e0fd51b80
EMME-ESSE <matimancini@gmail.com>
2022-10-25 05:45:44 -0700

    task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,17 @@
-#include "main.h"
+#include"main.h"
 /**
- *
- *
- *
-*/
+ * malloc_checked - function that allocates memory using malloc.
+ * @b: the parmether.
+ * Return: 0;
+ **/
 void *malloc_checked(unsigned int b)
+{
+	void *n;
 
+	n = malloc(b);
+	if (n == NULL)
+	{
+		exit(98);
+	}
+	return (n);
+}
