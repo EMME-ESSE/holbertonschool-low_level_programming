cb15005b77eaef9f99a646635f5faac160227106
EMME-ESSE <matimancini@gmail.com>
2022-10-26 12:49:14 -0700

    .

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -8,30 +8,35 @@
 **/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *pointer;
-	unsigned int i;
-	int size;
-	unsigned int sizes1 = 0;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	size = n;
-	if (n > strlen(s2))
-		size = strlen(s2);
-	pointer = malloc(sizeof(char)*(size + strlen(s1)) + 1);
-	if (pointer == NULL)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
 		return (NULL);
-	for (i = 0; s1[i] != '\0' ; i++)
+
+	while (i < len1)
 	{
-		pointer[i] = s1[i];
-		sizes1++;
+		s[i] = s1[i];
+		i++;
 	}
-	for (i = 0; i < n && s2[i] != '\0'; i++)
-	{
-		pointer[sizes1++] = s2[i];
-	}
-	pointer[i] = '\0';
-	return (pointer);
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
