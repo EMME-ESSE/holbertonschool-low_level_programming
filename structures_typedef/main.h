971e0e57eec968c523dcb9bbd51c709d3a475f9c
EMME-ESSE <matimancini@gmail.com>
2022-10-27 06:28:13 -0700

    .

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -6,5 +6,7 @@ char *name;
 float age;
 char *owner;
 };
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
