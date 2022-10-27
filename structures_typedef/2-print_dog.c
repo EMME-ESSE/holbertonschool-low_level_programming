7c63445b1395c37586d955607a3c937670858d5f
EMME-ESSE <matimancini@gmail.com>
2022-10-27 07:04:51 -0700

    task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -4,15 +4,15 @@
 /**
  *  * print_dog - Prints a struct dog.
  *   * @d: The struct dog to be printed.
- *    */
+**/
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-	else 
-		printf("Name:	%s\n", d->name);
+	else
+		printf("Name: %s\n", d->name);
 	if (d->age < 0)
 		printf("Age: (nil)\n");
 	else
