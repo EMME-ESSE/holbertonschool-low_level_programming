3722ca2e74e3d8dfc4a889ddbad75f9c03d9af4d
EMME-ESSE <matimancini@gmail.com>
2022-10-27 06:20:04 -0700

    task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,6 +11,11 @@
 **/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 {
-	struct dog my_dog;
-};
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
 #endif
