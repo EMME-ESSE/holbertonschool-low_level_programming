6c729500b32c25ca548abbda46857b02f10004fe
EMME-ESSE <matimancini@gmail.com>
2022-10-28 06:41:04 -0700

    .

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,9 +1,12 @@
 #include "function_pointers.h"
 /**
- *
+ * print_name - function that prints a name
+ * @name: the name to be printed
+ * @f: a pointer to the function
 **/
-
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
