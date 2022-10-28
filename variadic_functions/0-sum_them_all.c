d88ffdecef8f01115e7a1bbb04ecf2323d9ce4ef
EMME-ESSE <matimancini@gmail.com>
2022-10-28 08:51:04 -0700

    task 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,10 +10,10 @@ int sum_them_all(const unsigned int n, ...)
 va_list nums;
 int sum;
 unsigned int i;
-va_start (nums, n);
+va_start(nums, n);
 sum = 0;
 for (i = 0; i < n; i++)
-sum += va_arg (nums, int);
-va_end (nums);
+sum += va_arg(nums, int);
+va_end(nums);
 return (sum);
 }
