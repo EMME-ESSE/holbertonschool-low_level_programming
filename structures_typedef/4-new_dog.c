cbfe3c4291ead009677983a60519750e0cbbca47
EMME-ESSE <matimancini@gmail.com>
2022-10-30 13:12:51 -0700

    .

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,1 +1,62 @@
+#include "dog.h"
+/**
+ * new_dog - function that creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: it returns a pointer
+**/
+
 dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newdog;
+
+	if (name != NULL && owner != NULL)
+	{
+		newdog = malloc(sizeof(dog_t));
+		if (newdog == NULL)
+		{
+			return (NULL);
+		}
+		newdog->name = _strdup(name);
+		if (newdog->name == NULL)
+		{
+			free(newdog);
+			return (NULL);
+		}
+		newdog->owner = _strdup(owner);
+		if (newdog->owner == NULL)
+		{
+			free(newdog->name);
+			free(newdog);
+			return (NULL);
+		}
+		newdog->age = age;
+	}
+	return (newdog);
+}
+
+
+/**
+ * _strdup - Duplicates a string
+ * Return: The new string
+ * @str: Initial String
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i;
+	char *string2;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	string2 = malloc(sizeof(char) * strlen(str) + 1);
+	if (string2 == NULL)
+		return (NULL);
+
+	for (i = 0; i <= strlen(str) + 1; i++)
+		string2[i] = str[i];
+	return (string2);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  ** struct dog - a structure with three elements
  ** @name: name
@@ -19,6 +20,7 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
+char *_strdup(char *str);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
