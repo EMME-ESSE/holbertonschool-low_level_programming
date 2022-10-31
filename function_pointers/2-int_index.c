b5455cc37a35856baf1868c8777ae68d1f0cf1e6
EMME-ESSE <matimancini@gmail.com>
2022-10-31 07:50:44 -0700

    task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,19 +1,20 @@
 #include "function_pointers.h"
 /**
  *int_index -  function that searches for an integer
- *
+ *@array: the primary array
+ *@size: the number of elements
+ *@cmp: pointer to the function to be used to compare values
 **/
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array == NULL)	
-		return;
-	if (size <= 0)
+	int i;
+
+	if (array == NULL || cmp == NULL)
 		return(-1);
-	if(cmp == NULL)
-		return;
-	while (size --> 0)
+	for (i = 0; i < size; i++)
 	{
-		cmp(array);
-		array++;
+		if (cmp(array[i]) != 0)
+			return(i);
 	}
+	return(-1);
 }
