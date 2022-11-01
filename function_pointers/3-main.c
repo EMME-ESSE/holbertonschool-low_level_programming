4a2b9a3cdda4485e4f332b451892f94c6c317017
EMME-ESSE <matimancini@gmail.com>
2022-11-01 12:28:44 -0700

    task .

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,4 +1,20 @@
 #include "3-calc.h"
 int main(int argc, char *argv[])
 {
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		return (99);
+	}
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		return (100);
+	}
 }
