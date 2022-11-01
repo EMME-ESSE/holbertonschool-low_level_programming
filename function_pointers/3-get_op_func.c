6a64c08195e4658ab5051b9e8fb2b71739c762c5
EMME-ESSE <matimancini@gmail.com>
2022-11-01 12:05:46 -0700

    .

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,4 +1,12 @@
-int (*get_op_func(char *s))(int, int)
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ *  * get_op_func - Select the correct operator
+ *   * Return: Operator
+ *    * @symb: Operator writed by the user
+ *     */
+int (*get_op_func(char *symb))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
