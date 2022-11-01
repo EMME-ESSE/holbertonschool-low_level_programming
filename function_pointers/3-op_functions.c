e508b731b3d6f9360fc4cbd3e88894bbca1c8a76
EMME-ESSE <matimancini@gmail.com>
2022-11-01 14:17:01 -0700

    task 3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,5 +1,16 @@
 #include "3-calc.h"
+
 int op_add(int a, int b)
+/**
+ *op_add - adds two integers
+ * op_sub - Substract two integers
+ * op_mul - Multiply two integers
+ * op_div - Calculates the division of two integers
+ * op_mod - Calculates the module two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Result
+**/
 {
 	return (a + b);
 }
