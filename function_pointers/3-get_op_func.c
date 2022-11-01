a57c5c8ec4d7f2bc0b709f7f3c46828f975be310
EMME-ESSE <matimancini@gmail.com>
2022-11-01 12:17:19 -0700

    t.

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -18,9 +18,9 @@ int (*get_op_func(char *symb))(int, int)
 	};
 	int i = 0;
 
-		while (ops[i].op != NULL && *(ops[i].op) != *s)
+		while (ops[i].op != NULL && *(ops[i].op) != *symb)
 				{
 							i++;
 								}
-			return (ops[i].f);
+			return (ops[i]);
 }
