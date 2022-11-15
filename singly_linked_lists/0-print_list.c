4e412bfd40027e3702db015c3d99658c2b91a622
EMME-ESSE <matimancini@gmail.com>
2022-11-15 11:47:50 -0800

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 /*
- * */
+ * print_list - function that prints all the elements of a list.
+ * @list_t: the list
+ * Return: returns the size
+*/
 size_t print_list(const list_t *h)
 {
 	size_t x = 0;
