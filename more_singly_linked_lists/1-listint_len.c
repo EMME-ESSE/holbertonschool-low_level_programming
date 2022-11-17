8ee3f3535a20bb2a96a4bffd3861f3a27be6f8a8
EMME-ESSE <matimancini@gmail.com>
2022-11-17 12:55:26 -0800

    task 1

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 /**
- *  * print_listint - returns the elements at the list.
+ *  * listint_len - returns the elements at the list.
  *   * @h: A pointer to the head of the list.
  *    * Return: The number of elements in the list.
 **/
 size_t listint_len(const listint_t *h)
 {
 	size_t nd = 0;
-	
+
 	while (h)
 	{
 		nd++;
