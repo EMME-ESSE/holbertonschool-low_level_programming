978c5e5331bfe8f19f9fad69cbcdbdfde3287745
EMME-ESSE <matimancini@gmail.com>
2022-11-18 20:07:19 -0800

    task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,8 +9,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0, mult = 1;
 	int len;
 
-	if (b == '\0')
-		return (0);
+	if (*b == '\0')
+		return (0);	
 	for (len = 0; b[len];)
 		len++;
 	for (len -= 1; len >= 0; len--)
