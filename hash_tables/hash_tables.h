257cfe67c53e83f3de1813558a981d2e87101796
EMME-ESSE <matimancini@gmail.com>
2022-11-24 11:11:34 -0800

    main.h file

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -10,12 +10,12 @@
  *     * The key is unique in the HashTable
  *      * @value: The value corresponding to a key
  *       * @next: A pointer to the next node of the List
- *        */
+**/
 typedef struct hash_node_s
 {
 	     char *key;
-	          char *value;
-		       struct hash_node_s *next;
+	     char *value;
+	     struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -23,13 +23,14 @@ typedef struct hash_node_s
  *   *
  *    * @size: The size of the array
  *     * @array: An array of size @size
- *      * Each cell of this array is a pointer to the first node of a linked list,
- *       * because we want our HashTable to use a Chaining collision handling
- *        */
+ * Each cell of this array is a pointer to -
+ * - the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+**/
 typedef struct hash_table_s
 {
-	     unsigned long int size;
-	          hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
 #endif
