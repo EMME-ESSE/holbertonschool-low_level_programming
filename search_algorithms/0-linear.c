35827f8f7fb211a620f0a88b73c434e0fea99790
EMME-ESSE <matimancini@gmail.com>
2023-01-02 22:28:44 -0800

    Prototypes file

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	for (i = 0; i <= array[size - 1]; i++)
 	{
-		printf("Value checked array[%d] = [%d]", i, array[i]);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
